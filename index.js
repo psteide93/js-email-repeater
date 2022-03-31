61d0223031d949516a1fa6f6dcb0c04d10b2d52e
Parnell <psteide@ford.com>
2022-03-31 14:28:41 -0400

    Add button command

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button")
+let output = document.querySelector("output")
+
+button.addEventListener("click", () => {
+const input = document.querySelector("input").value
+    output.textContent = input} )
